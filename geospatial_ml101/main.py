cddc38cc6cf994489673a356561f9fecf6aca2a0
Johannes Plambeck <jooglyp@gmail.com>
2019-09-14 17:47:31 -0600

    added main.py and logger

---
+++ geospatial_ml101/main.py
@@ -0,0 +1,30 @@
+"""The main entrypoints live here."""
+import logging
+
+import numpy
+import geopandas
+
+from . import log, model
+
+LOGGER = logging.getLogger(__name__)
+
+
+def main():
+    log.init()
+
+    # Assignment Simulation:
+    LOGGER.info("Loading data from disk...")
+
+    with open("/tmp/data.csv", "r") as fileobj:
+        raw_data = geopandas.read_csv(fileobj)
+        X = raw_data[raw_data.columns.difference(["is_bad"])]
+        y = numpy.array(raw_data[["is_bad"]])
+
+    xgboost_model = model.XGBoostModel()
+
+    LOGGER.info(xgboost_model.evaluate(X, y))
+    # Assuming X is a pandas dataframe...
+    # TODO: Make note that dask is not talking to local threads very well. Not an actual error.
+    LOGGER.info(
+        xgboost_model.predict_proba(geopandas.DataFrame(xgboost_model.model.X.compute()))
+    )
